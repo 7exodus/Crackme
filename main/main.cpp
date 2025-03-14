ee1ee40d59fdb21d87e0cfaf2ce1d6db7f36dac0
7exodus <alphacharlie.2077@gmail.com>
2025-03-15 00:56:31 +0100

    Relase

---
+++ main/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+//sdk
+
+#include "sdk.hpp"
+
+std::string p;
+
+int main()
+{
+	Info();
+	return 0;
+}
---
+++ main/sdk.hpp
@@ -0,0 +1,34 @@
+void subx91823()
+{
+	system("cls");
+	std::cout << "I'm glad you've gained more experience";
+	system("timeout /t 1");
+}
+
+void subx123()
+{
+	std::string p;
+	std::cout << "Enter the key -> ";
+	std::cin >> p;
+	if (p == "Flag{n1c3-j0b-st8d3nt}")
+	{
+		system("tiemout /3");
+		subx91823();
+	}
+	else
+	{
+		std::cout << "u can do better next time";
+	}
+
+}
+
+void Info()
+{
+	system("color 2");
+	std::cout << "Welcome 7exodus Crackme" << std::endl;
+	std::cout << "This crackme is 1.2/5 lvl" << std::endl;
+	std::cout << "no packed and no protect version";
+	system("timeout /t 3");
+	system("cls");
+	subx123();
+}
