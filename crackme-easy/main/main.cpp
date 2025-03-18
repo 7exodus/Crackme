a6bc9d6daf80f1e93498a87d1d9f2359d1b6e41e
7exodus <alphacharlie.2077@gmail.com>
2025-03-18 15:53:28 +0100

    the code structure has been improved

---
+++ crackme-easy/main/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+#include "sdk.hpp"
+
+
+
+int main()
+{
+	// login
+	subx123();
+
+	return 0;
+}
---
+++ crackme-easy/main/sdk.hpp
@@ -0,0 +1,19 @@
+void subx123()
+{
+	std::string p;
+
+
+	std::cout << "Enter the key -> ";
+	std::cin >> p;
+
+	if (p == "Flag{n1c3-j0b-st8d3nt}")
+	{
+		std::cout << "succes!";
+		system("pause");
+	}
+	else
+	{
+		std::cout << "u can do better next time";
+	}
+
+}
